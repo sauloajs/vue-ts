c260561b2ab1bce1e8847f78665855f272c1343c
Saulo Silva <saulo.silva@smartload.com.br>
2023-02-27 16:49:37 -0300

    Omit interfaces

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,26 +1,56 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-const periods = ["Today", "This Week", "This Month"];
+import { DateTime } from 'luxon';
+import { computed, ref } from 'vue';
+import { TimeLinePost, today, thisMonth, thisWeek } from '../posts';
 
-const selectedPeriod = ref("Today");
+import TimeLineItem from './TimeLineItem.vue';
 
-function selectPeriod(period: string) {
+const periods = ["Today", "This Week", "This Month"] as const;
+
+type Period = typeof periods[number]; 
+
+const selectedPeriod = ref<Period>("Today");
+
+function selectPeriod(period: Period) {
     selectedPeriod.value = period;
 }
+
+const posts = computed<TimeLinePost[]>(() => [
+    today,
+    thisWeek,
+    thisMonth
+].map(post => {
+    return {
+        ...post,
+        created: DateTime.fromISO(post.created)
+    }
+}).filter(post => {
+    if (selectedPeriod.value === "Today") {
+        return post.created >= DateTime.now().minus({ day: 1 })
+    }
+    
+    if (selectedPeriod.value === "This Week") {
+        return post.created >= DateTime.now().minus({ week: 1 })
+    }
+
+    return post
+}))
 </script>
 
 <template>
   <nav class="is-primary panel">
-    {{ selectedPeriod }}
     <div class="panel-tabs">
       <a 
         v-for="period of periods" 
         :key="period" 
         :href="period"
+        :class="{ 'is-active': period === selectedPeriod }"
         @click.prevent="selectPeriod(period)"
     >
         {{ period }}
       </a>
     </div>
+
+    <TimeLineItem v-for="post of posts" :key="post.id" :post="post" />
   </nav>
 </template>
