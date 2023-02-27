24981f06414bad98b6208956cb8acaebb8f707a0
Saulo Silva <saulo.silva@smartload.com.br>
2023-02-27 15:58:01 -0300

    vue directives

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
+const periods = ["Today", "This Week", "This Month"];
 </script>
 
 <template>
-    <nav class="is-primary panel">
-        <div class="panel-tabs">
-            <a href="">Today</a>
-            <a href="">This Week</a>
-            <a href="">This Month</a>
-        </div>
-    </nav>
+  <nav class="is-primary panel">
+    <div class="panel-tabs">
+      <a v-for="period of periods" :key="period" :href="period">{{ period }}</a>
+    </div>
+  </nav>
 </template>
