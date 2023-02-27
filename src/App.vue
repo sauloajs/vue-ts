1003760630c3f61e5ff9bc489fe5835c055bf7cb
Saulo Silva <saulo.silva@smartload.com.br>
2023-02-27 15:54:27 -0300

    creating ts project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TimeLine from './components/TimeLine.vue';
+</script>
+
+<template>
+  <div class="section">
+    <div class="container">
+      <TimeLine />
+    </div>
+  </div>
+</template>
+
+<style>
+@import "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css";
+</style>
---
+++ src/components/TimeLine.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <nav class="is-primary panel">
+        <div class="panel-tabs">
+            <a href="">Today</a>
+            <a href="">This Week</a>
+            <a href="">This Month</a>
+        </div>
+    </nav>
+</template>
